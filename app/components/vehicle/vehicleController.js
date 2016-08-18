bd4eca6eb551466f1292b048e6d43b3444a41399
Alexandre Silveira <alexandre.sneto@gmail.com>
2016-08-17 23:32:21 -0300

    Ajustes

--- app/components/vehicle/vehicleController.js
+++ app/components/vehicle/vehicleController.js
@@ -1,29 +1,5 @@
-angular.module('appControllers').controller('VeiculosCtrl', ['$scope', 'ProcurarVeiculo', 'Veiculo', 'MeuVeiculo', 'Login',
-  function ($scope, ProcurarVeiculo, Veiculo, MeuVeiculo, Login) {
+angular.module('appControllers').controller('VehicleCtrl', ['$scope', 
+  function ($scope) {
 
-  ProcurarVeiculo.get({}, function (data) {
-    console.log(data);
-      $scope.veiculos = data.content;
-  });
 
-  MeuVeiculo.get({}, function (data) {
-    console.log(data);
-  });
-
-  $scope.criarVeiculo = function () {
-    var carro = {
-      placa: "DDD0404",
-      marca: "Tesla",
-      modelo: "Model X",
-      cor: "Preto",
-      proprietario: {
-        nome: "Lucas Balensiefer",
-        ramal: "640",
-        celular: "(51) 9997-9997",
-        posicao: "4-370"
-      }
-    };
-
-    Veiculo.save({id: carro.placa}, carro);
-  };
 }]);
