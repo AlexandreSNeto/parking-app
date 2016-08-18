f2de43fb37ccecb58a162941cc15288036803324
Alexandre Silveira <alexandre.sneto@gmail.com>
2016-08-17 23:28:29 -0300

    Ajustes no componente de busca

--- app/components/search/searchController.js
+++ app/components/search/searchController.js
@@ -1,4 +1,16 @@
-angular.module('appControllers').controller('SearchCtrl', ['$scope',
-  function ($scope) {
+angular.module('appControllers').controller('SearchCtrl', ['$scope', 'SearchVehicle', 
+  function ($scope, SearchVehicle) {
+
+  $scope.licensePlate = "i";
+
+  $scope.$watch('licensePlate', function (value) {
+    searchLicensePlate();
+  });
+
+  var searchLicensePlate = function () {
+    SearchVehicle.get({placa: $scope.licensePlate}, function (data) {
+      $scope.veiculos = data.content;
+    });
+  };
 
 }]);
