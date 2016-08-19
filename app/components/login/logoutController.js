3e75e61230f09197341097ef7a1a633c0958c4e0
Alexandre Silveira <alexandresilveira@cwi.com.br>
2016-08-19 17:00:00 -0300

    Criação do controller de Logout

---
+++ app/components/login/logoutController.js
@@ -0,0 +1,11 @@
+'use strict';
+angular.module('appControllers').controller('LogoutCtrl', ['$scope', 'AuthLogout', '$location', '$rootScope',
+function ($scope, AuthLogout, $location, $rootScope) {
+
+    AuthLogout.get({}, function (data) {
+        $rootScope.user = {};
+        $location.path('/login');
+    });
+
+}]);
+
