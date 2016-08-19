7bd2b471c38b5bb9e6f36f4a7edb1b107c95a1a4
Alexandre Silveira <alexandresilveira@cwi.com.br>
2016-08-19 17:01:28 -0300

    Método para cancelar a ação

--- app/components/vehicle/addVehicleController.js
+++ app/components/vehicle/addVehicleController.js
@@ -1,15 +1,21 @@
-angular.module('appControllers').controller('AddVehicleCtrl', ['$scope', '$rootScope', 'Vehicle',
-  function ($scope, $rootScope, Vehicle) {
+'use strict';
+angular.module('appControllers').controller('AddVehicleCtrl', ['$scope', '$rootScope', '$location', 'Vehicle',
+  function ($scope, $rootScope, $location, Vehicle) {
 
-    $scope.salvar = function () {
+    $scope.save = function () {
       $scope.vehicle.proprietario = {
-        id: $rootScope.owner.id
+        id: $rootScope.user.id
       };
 
       Vehicle.save($scope.vehicle, function (data) {
+        $scope.vehicle = {};
         $scope.sucesso = true;
         $scope.mensagem = "Veículo adicionado."
       });
+    };
+
+    $scope.cancel = function () {
+      $location.path('/vehicle');
     };
 
   }]);
