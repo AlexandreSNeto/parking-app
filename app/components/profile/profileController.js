7eb3ba0b0d2f95086ea2a7173496478acd77ae9d
Alexandre Silveira <alexandre.sneto@gmail.com>
2016-08-21 22:54:11 -0300

    Exibição de mensagens

--- app/components/profile/profileController.js
+++ app/components/profile/profileController.js
@@ -1,19 +1,32 @@
 'use strict';
 angular.module('appControllers').controller('ProfileCtrl', ['$scope', 'Owner',
-  function ($scope, Owner) {
+    function ($scope, Owner) {
 
-    Owner.get({}, function (data) {
-      $scope.owner = data;
-    });
+        Owner.get({}, function (data) {
+            $scope.owner = data;
+        });
 
-    $scope.salvar = function () {
-      if (!$scope.profileForm.$valid) {
-        return;
-      }
-      Owner.save($scope.owner, function (data) {
-        $scope.sucesso = true;
-        $scope.mensagem = "Perfil atualizado."
-      });
-    };
+        $scope.salvar = function () {
+            if (!$scope.profileForm.$valid) {
+                return;
+            }
+            Owner.save($scope.owner, function () {
+                success('Perfil atualizado.');
+            }, function (failData) {
+                fail('Houve um problema ao atualizar o perfil: ' + failData.data.message);
+            });
+        };
 
-}]);
+        var success = function (message) {
+            $scope.message = message;
+            $scope.success = true;
+            $scope.fail = false;
+        };
+
+        var fail = function (message) {
+            $scope.message = message;
+            $scope.fail = true;
+            $scope.success = false;
+        };
+
+    }]);
