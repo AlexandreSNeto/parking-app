bc114eba3a79d30342f06f46bff150fd64f1fe79
Alexandre Silveira <alexandre.sneto@gmail.com>
2016-08-23 22:51:52 -0300

    Incluindo mensagens de retorno no login

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -1,25 +1,40 @@
 'use strict';
 angular.module('appControllers').controller('LoginCtrl', ['$scope', 'AuthLogin', 'Owner', '$httpParamSerializer', '$location', '$rootScope',
-  function ($scope, AuthLogin, Owner, $httpParamSerializer, $location, $rootScope) {
+    function ($scope, AuthLogin, Owner, $httpParamSerializer, $location, $rootScope) {
 
-    
+        $scope.entrar = function () {
+            AuthLogin.login($httpParamSerializer($scope.auth), function (data) {
+                Owner.get({}, function (data) {
+                    $rootScope.user = {
+                        id: data.id,
+                        name: data.nome,
+                        avatar: data.gravatar
+                    };
 
-    $scope.entrar = function () {
-      AuthLogin.login($httpParamSerializer($scope.auth), function (data) {        
-        Owner.get({}, function (data) {
-          $rootScope.user = {
-            id: data.id,
-            name: data.nome,
-            avatar: data.gravatar
-          };
+                    if (!data.celular || !data.ramal) {
+                        $location.path('/profile');
+                    } else {
+                        $location.path('/');
+                    }
+                });
+            }, function (failData) {
+                console.log('Fail!');
+                fail('Login não realizado. ' + failData.data.message);
+            });
+        };
 
-          if (!data.celular || !data.ramal) {
-            $location.path('/profile');
-          } else {
-            $location.path('/');
-          }
-        });       
-      });
-    };
-    
-}]);
+        var success = function (message, messageHeader) {
+            $scope.messageHeader = messageHeader ? messageHeader : 'Sucesso!';
+            $scope.message = message;
+            $scope.success = true;
+            $scope.fail = false;
+        };
+
+        var fail = function (message, messageHeader) {
+            $scope.messageHeader = messageHeader ? messageHeader : 'Ops!';
+            $scope.message = message;
+            $scope.fail = true;
+            $scope.success = false;
+        };
+
+    }]);
