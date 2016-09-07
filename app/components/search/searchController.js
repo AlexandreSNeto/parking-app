aa9b7ab96e11d0df528263bdf762175f52d18201
Alexandre Silveira <alexandre.sneto@gmail.com>
2016-09-07 12:06:50 -0300

    delay de 400ms na consulta para evitar multiplas chamadas ao backend enquanto estiver digitando uma placa

--- app/components/search/searchController.js
+++ app/components/search/searchController.js
@@ -1,6 +1,6 @@
 'use strict';
-angular.module('appControllers').controller('SearchCtrl', ['$scope', 'SearchVehicle',
-    function ($scope, SearchVehicle) {
+angular.module('appControllers').controller('SearchCtrl', ['$scope', 'SearchVehicle', '$timeout',
+    function ($scope, SearchVehicle, $timeout) {
 
         // Pagination
         $scope.pager = {
@@ -11,23 +11,28 @@ angular.module('appControllers').controller('SearchCtrl', ['$scope', 'SearchVehi
             page: 1
         };
         $scope.changePage = function () {
-            search();
+            search($scope.licensePlate);
         };
 
         $scope.licensePlate = "";
         $scope.veiculos = [];
 
+        var timeoutPromise;
         $scope.$watch('licensePlate', function (value) {
+            var licensePlate = $scope.licensePlate;
+            $timeout.cancel(timeoutPromise);
             if ($scope.licensePlate != '' && $scope.licensePlate.length >= 2) {
                 $scope.pager.page = 1;
-                search();
+                timeoutPromise = $timeout(function () {
+                    search(licensePlate);
+                }, 400);
             } else {
                 $scope.veiculos = [];
             }
         });
 
-        var search = function () {
-            $scope.searchPromise = SearchVehicle.get({ placa: $scope.licensePlate, page: $scope.pager.page - 1, size: $scope.pager.size }, function (data) {
+        var search = function (licensePlate) {
+            $scope.searchPromise = SearchVehicle.get({ placa: licensePlate, page: $scope.pager.page - 1, size: $scope.pager.size }, function (data) {
                 $scope.veiculos = data.content;
                 $scope.pager.totalElements = data.totalElements;
                 $scope.pager.totalPages = data.totalPages;
